f5dccff26d9f6fee7f328dc9972ca2cbefb68c7e
Ivan <razorcol98@gmail.com>
2017-11-02 23:15:04 -0700

    v 1.2

--- 8ball.rb
+++
@@ -1,9 +0,0 @@
-begin
-	answers = File.readlines('./files/answers.txt').sample
-	greetings = File.readlines('./files/greetings.txt').sample
-	puts greetings
-	sleep(3)
-	puts answers
-rescue
-	puts "Видимо один или оба файла не найдены :("
-end
